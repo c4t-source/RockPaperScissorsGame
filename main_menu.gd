3fa6888fbdf0d2e7d0f83d7d2182b6eaba100db1
Nadovi-game <mail2.nadav@gmail.com>
2026-02-01 20:54:34 -0500

    added menu
    hi

---
+++ main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://node_2d.tscn")
+	print("hi")
+
+
+func _on_options_2_pressed() -> void:
+	print("hi")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
