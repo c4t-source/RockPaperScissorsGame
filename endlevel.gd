189de54edb92fd6772563cea913cd50764f340a7
Nadovi-game <mail2.nadav@gmail.com>
2026-02-03 22:14:03 -0500

    added a pause.

--- endlevel.gd
+++ endlevel.gd
@@ -1,13 +1,11 @@
 extends Area2D
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		await get_tree().create_timer(1.5).timeut
 		var current_scene = get_tree().current_scene.scene_file_path
-		var file_name = current_scene.get_file() # "Level_1.tscn"
-		
+		var file_name = current_scene.get_file()
 		var level_number = file_name.get_basename().split("_")[1].to_int()
 		var next_level = level_number + 1
-		
-		var next_level_dest = "res://levels/Level_" + str(next_level) + ".tscn"
-		get_tree().change_scene_to_file(next_level_dest)
+		var next_level_destin = "res://levels/Level_" + str(next_level) + ".tscn"
+		get_tree().change_scene_to_file(next_level_destin)
