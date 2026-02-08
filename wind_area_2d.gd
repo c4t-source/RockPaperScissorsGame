e780eec7f8c3af92aaeeaf7fa9737edbc49c1642
c4t-source <hilleldulev@gmail.com>
2026-02-08 14:40:12 -0500

    dumbass fan

---
+++ wind_area_2d.gd
@@ -0,0 +1,18 @@
+extends Area2D
+class_name WindArea2D
+@export var speed: Vector2 = Vector2.UP
+
+func _ready() -> void:
+	body_entered.connect(_object_entered)
+	body_exited.connect(_object_exited)
+
+func _object_entered(object: Node2D) -> void: 
+	if "wind_velocity" in object:
+		if speed.y < 0.0:
+			object.velocity.y = speed.y
+		object.wind_velocity += speed
+
+func _object_exited(object: Node2D) -> void:
+	if "wind velocity" in object:
+		object.wind_velocity -= speed
+	
