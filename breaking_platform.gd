e8ae357dc2683b32ca2f5f4b2fb5698ac18d4a51
c4t-source <hilleldulev@gmail.com>
2026-02-15 18:25:35 -0500

    made level 1 better

--- breaking_platform.gd
+++ breaking_platform.gd
@@ -2,8 +2,12 @@ extends Sprite2D
 
 var has_fallen: bool = false
 
+
+enum CharacterType { X, Y, Z }
+var current_character: CharacterType
+
 func _on_area_2d_body_entered(body):
-	if body is CharacterBody2D and !has_fallen:
+	if body is CharacterBody2D and !has_fallen and current_character != CharacterType.Y and current_character != CharacterType.Z:
 		var opacity_tween: Tween = create_tween().set_trans(Tween.TRANS_SINE)
 		var pos_tween: Tween = create_tween().set_trans(Tween.TRANS_SINE)
 		 
