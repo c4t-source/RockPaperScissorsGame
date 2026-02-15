b3c459c2e24946125fc3ee912d73dd7c066c5791
Nadovi-game <mail2.nadav@gmail.com>
2026-02-14 20:51:41 -0500

    I DID IT

--- scripts/vertical_fan.gd
+++ scripts/vertical_fan.gd
@@ -1,17 +1,17 @@
 extends Area2D
 class_name WindArea2D
 
-@export var speed: Vector2 = Vector2(0, -400)
+@export var speed: Vector2 = Vector2(0, -2000)
 
 
 
-func _physics_process(delta):
-	for body in get_overlapping_bodies():
-		if "wind_velocity" in body:
-			if speed.y < 0.0:
-				body.velocity.y = speed.y
-			body.wind_velocity += speed * delta
 
-func _body_exited(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+	if "wind_velocity" in body:
+		if speed.y < 0.0:
+			body.velocity.y = speed.y
+		body.wind_velocity += speed
+
+func _on_body_exited(body: Node2D) -> void:
 	if "wind_velocity" in body:
 		body.wind_velocity -= speed
