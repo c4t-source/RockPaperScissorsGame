7c063edd429063b31221180559846e54c792039e
Nadovi-game <mail2.nadav@gmail.com>
2026-03-05 21:53:21 -0500

    fire alarm chirp

--- scripts/cardboard.gd
+++ scripts/cardboard.gd
@@ -3,8 +3,10 @@ extends Area2D
 @onready var collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
 @onready var collision_shape_2dbreak: CollisionShape2D = $CollisionShape2D
 @onready var player = $"../../CharacterBody2D"
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 func _on_body_entered(body: Node2D) -> void:
 	if player.current_character == player.CharacterType.Y:
 		$Sprite2D.visible = false
 		$StaticBody2D/CollisionShape2D.queue_free()
 		$CollisionShape2D.queue_free()
+		audio_stream_player_2d.play()
