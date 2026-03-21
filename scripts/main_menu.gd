261e62e1fecae424f54e76028a3c86d401cd106e
Nadovi-game <mail2.nadav@gmail.com>
2026-03-20 21:39:17 -0400

    check

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,14 +1,25 @@
 extends Control
 
+@onready var menubuttons: VBoxContainer = $Menubuttons
+@onready var options: Panel = $options
 
 
+func _ready():
+	menubuttons.visible = true
+	options.visible = false
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/Level_1.tscn")
 
 
 func _on_options_2_pressed() -> void:
 	print("hi")
+	menubuttons.visible = false
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	_ready()
