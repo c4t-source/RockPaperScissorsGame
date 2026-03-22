d0bbd23f63aeb8636986628a083ff0f178d08c01
Nadovi-game <mail2.nadav@gmail.com>
2026-03-21 20:15:46 -0400

    .

--- scenes/other scenes/audioslider.gd
+++ scenes/other scenes/audioslider.gd
@@ -5,7 +5,7 @@ extends HSlider
 var audio_bus_id
 
 func _ready():
-	audio_bus_id = AudioServer.get_bus_index("SFX")
+	audio_bus_id = AudioServer.get_bus_index("Music")
 
 func _on_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
