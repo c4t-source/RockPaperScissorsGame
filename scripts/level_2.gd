c71a5c907a02da9610d3cb4cd30f09de689e0b7f
c4t-source <hilleldulev@gmail.com>
2026-03-22 22:18:35 -0400

    started Level 2/ Level 1 after tutorial

---
+++ scripts/level_2.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready():
+	TransitionFade.fade(0.0, 1.0)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,12 +3,20 @@ extends Control
 @onready var menubuttons: VBoxContainer = $Menubuttons
 @onready var options: Panel = $options
 @onready var transition_fade = $TransitionFade
+@onready var levels = $Levels
+@onready var TutorialB = $Levels/TutorialB
+@onready var level1_B = $Levels/Level1B
 
 
 func _ready(): 
 	menubuttons.visible = true
+	levels.visible = true
+	TutorialB.visible = false
+	level1_B.visible = false
 	options.visible = false
+	
 	await TransitionFade.fade(0.0, 0.5).finished
+	
 
 func _on_start_pressed() -> void:
 	await TransitionFade.fade(1.0, 1.0).finished 
@@ -28,3 +36,19 @@ func _on_exit_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	_ready()
+
+
+func _on_levels_pressed():
+	TutorialB.visible = true
+	level1_B.visible = true
+
+func _on_tutorial_b_pressed():
+	await TransitionFade.fade(1.0, 1.0).finished 
+	TransitionFade.color_rect.color.a = 1.0
+	get_tree().change_scene_to_file("res://scenes/levels/Level_1.tscn")
+
+
+func _on_level_1b_pressed():
+	await TransitionFade.fade(1.0, 1.0).finished 
+	TransitionFade.color_rect.color.a = 1.0
+	get_tree().change_scene_to_file("res://scenes/levels/Level_2.tscn")
