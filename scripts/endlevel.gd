26e74e722d05c43c02d67a8f4a6e9bce39801065
Nadovi-game <mail2.nadav@gmail.com>
2026-03-26 23:07:35 -0400

    ..

--- scripts/endlevel.gd
+++ scripts/endlevel.gd
@@ -1,7 +1,11 @@
 extends Area2D
 signal player_finished_level
-
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+func _ready():
+	audio_stream_player_2d.volume_db = -80
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		audio_stream_player_2d.volume_db = 0
 		get_tree().paused = true
 		player_finished_level.emit()
+		audio_stream_player_2d.play()
--- scripts/level_1_script.gd
+++ scripts/level_1_script.gd
@@ -1,5 +1,7 @@
 extends Node2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _ready():
 	TransitionFade.fade(0.0, 1.0)
+	audio_stream_player_2d.play()
